855f4e110fe8a955ca930aac0cb0ae848a677058
Vonara Wijethunge <vonarawijethunge@gmail.com>
2026-02-23 21:21:54 +0530

    Implement file reading functionality and add example text file

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Node.js project setup successful!");
+
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+ if (err) throw err;
+ console.log(data);
+});
