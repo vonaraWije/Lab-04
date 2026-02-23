80fc9f8d4e929008517f145c96eb11bc19baf374
Vonara Wijethunge <vonarawijethunge@gmail.com>
2026-02-23 21:42:52 +0530

    Replace file reading functionality with HTTPS request to fetch data from external API

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-console.log("Node.js project setup successful!");
-
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+ let data = '';
+ resp.on('data', (chunk) => {
+ data += chunk;
+ });
+ resp.on('end', () => {
+ console.log(JSON.parse(data));
+ });
+}).on('error', (err) => {
+ console.log("Error: " + err.message);
 });
